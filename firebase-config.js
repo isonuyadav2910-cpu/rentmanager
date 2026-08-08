3a16da22a9131266e473ae7d4e1ad7df8cb62b2c
isonuyadav2910-cpu <isonuyadav2910@gmail.com>
2026-08-08 20:32:12 +0530

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,10 +4,10 @@
 //
 // Do not add quotes around the file itself; the values must remain strings.
 
-windo apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_STORAGE_BUCKET",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+ apiKey: "AIzaSyBZdB8odEXTzC9kFP6Ft3PKCeAhd8WTnWU",
+  authDomain: "rent-manager-11-rooms.firebaseapp.com",
+  projectId: "rent-manager-11-rooms",
+  storageBucket: "rent-manager-11-rooms.firebasestorage.app",
+  messagingSenderId: "381338899569",
+  appId: "1:381338899569:web:ea62601c31bd5ec2b677d0",
 };
