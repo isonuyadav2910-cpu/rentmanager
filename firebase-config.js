c9964b7ec7bd7798d44b09b464606c70a1f72a0f
isonuyadav2910-cpu <isonuyadav2910@gmail.com>
2026-08-09 12:11:14 +0530

    Update Firebase configuration for RentManager

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
-// Firebase configuration for RentManager.
-// Firebase Web App configuration from the RentManager Firebase project.
-window.FIREBASE_CONFIG = {
+const firebaseConfig = {
   apiKey: "AIzaSyBZdB8odEXTzC9kFP6Ft3PKCeAhd8WTnWU",
   authDomain: "rent-manager-11-rooms.firebaseapp.com",
   projectId: "rent-manager-11-rooms",
   storageBucket: "rent-manager-11-rooms.firebasestorage.app",
   messagingSenderId: "381338899569",
-  appId: "1:381338899569:web:ea62601c31bd5ec2b677d0"
+  appId: "1:381338899569:web:bc0edbe266032d28b677d0",
+  measurementId: "G-3CH0PGPSMB"
 };
+
+export default firebaseConfig;
